8ded5e10b8dff57800a65c11abf8af434b223e3b
TMANVIT <TManshin@yandex.ru>
2024-06-15 00:14:26 +0300

    done YOLO at 2500 epochs

--- YOLO_model/learn.py
+++ YOLO_model/learn.py
@@ -4,4 +4,4 @@ model = YOLO("yolov8n.yaml")
 
 
 if __name__ == '__main__':
-    results = model.train(data="YOLO_model\data.yaml", epochs=500, patience=0)
+    results = model.train(data="YOLO_model\data.yaml", epochs=2500, patience=0)
